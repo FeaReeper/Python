d678a9fa706297181a29483212dda103e722dfc3
Matthew Reep <reepmatt20@gmail.com>
2023-07-16 19:20:57 -0400

    card game added

---
+++ fundamentals/oop/hack_a_thon/card_game/game.py
@@ -0,0 +1,12 @@
+from classes.deck import Deck
+
+
+
+hi_or_low = Deck()
+
+# bicycle.show_cards()
+
+# Allow a player to pick a random card from the deck, and the computer to pick a random card from the deck
+
+# Create a conditional that if the player card is higher, he wins and gets the point value of the card he played, same for the computer
+
